1933af84e7762a6b30a61859b0050f3ecc202c42
Dave Perry <dave@slugburn.com>
2012-09-16 23:13:09 -0700

    Stubbing out curses

--- Slugburn.Thunderstone.Lib/Curse.cs
+++ Slugburn.Thunderstone.Lib/Curse.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Slugburn.Thunderstone.Lib.Abilities;
 using Slugburn.Thunderstone.Lib.Events;
 using Slugburn.Thunderstone.Lib.Randomizers;
+using Slugburn.Thunderstone.Lib.Selectors;
 
 namespace Slugburn.Thunderstone.Lib
 {
@@ -19,7 +20,7 @@ namespace Slugburn.Thunderstone.Lib
 
         public IEnumerable<Card> CreateCards(Game game)
         {
-            return CreateCurses(game, Decay, Hostility);
+            return CreateCurses(game, Decay, Hostility, Neglect, Discord, Horror, Shame, Sloth);
         }
 
         private IEnumerable<Card> CreateCurses(Game game, params Action<Card>[] modifications)
@@ -43,7 +44,66 @@ namespace Slugburn.Thunderstone.Lib
                 );
         }
 
-        private void Decay(Card card)
+        private static void War(Card card)
+        {
+            card.Name = "Curse of War";
+            card.Text = "You cannot rest or prepare."
+                        + "<br/><br/>"
+                        + "<b>Trophy:</b> Attack -1"
+                        + "<br/><br/>" + "<b>Village/Dungeon:</b> Select 2 random cards from your hand <i>(excluding this card).</i>  " 
+                        + "You may destroy 1 of them to destroy this card.";
+            // TODO: Implement
+        }
+
+        private static void Sloth(Card card)
+        {
+            card.Name = "Curse of Sloth";
+            card.Text +=
+                "<small><b>Dungeon:</b> Destroy this curse. Lower your Total Attack Value by 3. " +
+                "You cannot equip any more weapons, use any other Dungeon abilities, or use any other Trophy effects this turn.</small>";
+            // TODO: Implement
+        }
+
+        private static void Shame(Card card)
+        {
+            card.Name = "Curse of Shame";
+            card.Text +=
+                "<b>Village/Dungeon:</b> Destroy this curse. Draw 2 fewer cards when you draw a new hand.";
+            // TODO: Implement
+        }
+
+        private static void Horror(Card card)
+        {
+            card.Name = "Curse of Horror";
+            card.Text +=
+                "<small><b>Dungeon:</b> Destroy this curse. Reduce your Light to 0. " 
+                + "You cannot equip any more weapons, use any other Dungeon abilities, or use any other Trophy effects this turn.</small>";
+            // TODO: Implement
+        }
+
+        private static void Discord(Card card)
+        {
+            card.Name = "Curse of Discord";
+            card.Text +=
+                "<b>Village:</b> Lose 2 gold to destroy this curse.";
+            // TODO: Implement
+        }
+
+        private static void Neglect(Card card)
+        {
+            card.Name = "Curse of Neglect";
+            card.Text +=
+                "<b>Village/Dungeon:</b> Discard 2 cards to destroy this curse.";
+            card.CreateAbility()
+                .Description("Discard 2 cards to destroy this curse.")
+                .SelectCards(source=>source.FromHand().Caption("Curse of Neglect").Message("Discard 2 cards.").Min(2).Max(2))
+                .OnCardsSelected(context => context.Player.DiscardCards(context.Selected))
+                .Action(player => player.DestroyCard(card, card.Name))
+                .Condition(player => player.Hand.Count >= 2)
+                .On(Phase.Village, Phase.Dungeon);
+        }
+
+        private static void Decay(Card card)
         {
             card.Name = "Curse of Decay";
             card.Text +=
