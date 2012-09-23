af0625126924fa53a3c92118c74abe3d79c36ff3
Dave Perry <dave@slugburn.com>
2012-09-23 03:05:51 -0700

    Finished Skinshifter

---
+++ Slugburn.Thunderstone.Lib/Debug/Assert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Slugburn.Thunderstone.Lib.Debug
+{
+    public static class Assert
+    {
+        public static void That(bool condition, string message)
+        {
+            if (!condition)
+                throw new Exception(message);
+        }
+    }
+}
